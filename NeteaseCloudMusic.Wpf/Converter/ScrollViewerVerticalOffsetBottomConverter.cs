a4c313099c46cdf94c4422af525c90e00e20447b
kumamiko <2573751144@qq.com>
2020-06-16 18:30:04 +0800

    添加平滑滚动

---
+++ NeteaseCloudMusic.Wpf/Converter/ScrollViewerVerticalOffsetBottomConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NeteaseCloudMusic.Wpf.Converter
+{
+    public class ScrollViewerVerticalOffsetBottomConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)values[0] + (double)values[1] - (double)values[2] >= 0 ? 0 : 1 ;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
