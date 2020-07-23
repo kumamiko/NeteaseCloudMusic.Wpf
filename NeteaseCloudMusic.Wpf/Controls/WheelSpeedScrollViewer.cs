7099dbd199527493b9d12cd0715f92cd3ff46bc5
kumamiko <2573751144@qq.com>
2020-07-23 20:33:32 +0800

    修改滚动条的速度，默认值太慢

---
+++ NeteaseCloudMusic.Wpf/Controls/WheelSpeedScrollViewer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace NeteaseCloudMusic.Wpf.Controls
+{
+    /// <summary>
+    /// 可调整鼠标灵敏度
+    /// https://stackoverflow.com/questions/1639505/wpf-scrollviewer-scroll-amount
+    /// </summary>
+    public class WheelSpeedScrollViewer : ScrollViewer
+    {
+        public static readonly DependencyProperty SpeedFactorProperty =
+            DependencyProperty.Register(nameof(SpeedFactor),
+                                        typeof(Double),
+                                        typeof(WheelSpeedScrollViewer),
+                                        new PropertyMetadata(2.5));
+
+        public Double SpeedFactor
+        {
+            get { return (Double)GetValue(SpeedFactorProperty); }
+            set { SetValue(SpeedFactorProperty, value); }
+        }
+
+        protected override void OnPreviewMouseWheel(MouseWheelEventArgs e)
+        {
+            if (!e.Handled &&
+                ScrollInfo is ScrollContentPresenter scp &&
+                ComputedVerticalScrollBarVisibility == Visibility.Visible)
+            {
+                scp.SetVerticalOffset(VerticalOffset - e.Delta * SpeedFactor);
+                e.Handled = true;
+            }
+        }
+    };
+}
